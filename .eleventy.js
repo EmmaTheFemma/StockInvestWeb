3cecd7b9e8d4053f37a102c59330eceeb2d015c3
EmmaTheFemma <92608727+EmmaTheFemma@users.noreply.github.com>
2022-03-21 16:08:57 +0100

    Basic SCSS
    Addes SCSS and some other cool stuff. Dates

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,17 @@
+const { DateTime } = require("luxon");
+
 // https://www.11ty.dev/docs/config/
 module.exports = function(eleventyConfig) {
 
     //Add files to the public folder.
-    eleventyConfig.addPassthroughCopy('./src/styles/style.css');
+    eleventyConfig.addPassthroughCopy('./src/sass/style.css');
+
+    //All date related.
+    //https://11ty.rocks/eleventyjs/dates/
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
     // Return your Object options:
     return {
