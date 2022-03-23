355f0fd5548ecafcfbcf82ffaa4c66023b99a3af
EmmaTheFemma <92608727+EmmaTheFemma@users.noreply.github.com>
2022-03-24 00:47:44 +0100

    Added hamburger
    + small fixes.

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+//Made from this tutorial:
+// https://www.youtube.com/watch?v=flItyHiDm7E&t=22s
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -6,8 +8,6 @@ hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 })
 
-//NOT DONE
-// https://www.youtube.com/watch?v=flItyHiDm7E&t=22s
 document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
